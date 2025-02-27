7807d5b7068587d02a8b4c4df971ea578d52877a
vishwadwivedi35 <68500903+vishwadwivedi35@users.noreply.github.com>
2025-02-27 11:30:23 +0530

    updated get route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
 app.post("/api/refer", async (req, res) => {
   try {
     const { referrerName, referrerEmail, refereeName, refereeEmail } = req.body;
